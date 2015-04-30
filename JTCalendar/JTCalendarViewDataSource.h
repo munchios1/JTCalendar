95cb04d8e2ff123d4165f46886b3b4f39fb073d8
munchios1 <princek@hungrybuzz.info>
2015-04-30 11:22:24 +0530

    have event protocol change to optional

--- JTCalendar/JTCalendarViewDataSource.h
+++ JTCalendar/JTCalendarViewDataSource.h
@@ -10,12 +10,10 @@
 @class JTCalendar;
 
 @protocol JTCalendarDataSource <NSObject>
-
-- (BOOL)calendarHaveEvent:(JTCalendar *)calendar date:(NSDate *)date;
 - (void)calendarDidDateSelected:(JTCalendar *)calendar date:(NSDate *)date;
-
 @optional
 
+- (BOOL)calendarHaveEvent:(JTCalendar *)calendar date:(NSDate *)date;
 - (BOOL)calendar:(JTCalendar *)calendar canSelectDate:(NSDate *)date;
 
 - (void)calendarDidLoadPreviousPage;
